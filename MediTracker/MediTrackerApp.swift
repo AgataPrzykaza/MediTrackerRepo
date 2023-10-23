a3bd85d0aa829948092d45ddbd03837ebc01bfba
AgataPrzykaza <agatprz668@student.polsl.pl>
2023-10-23 18:23:20 +0200

    Initial Commit

---
+++ MediTracker/MediTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediTrackerApp.swift
+//  MediTracker
+//
+//  Created by Agata Przykaza on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MediTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
